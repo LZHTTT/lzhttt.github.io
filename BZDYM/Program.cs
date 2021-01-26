0020e89c776d0571db0fc4c406e0750777835138
lzhttt <1941083483@qq.com>
2021-01-26 15:20:29 +0800

    Site updated: 2021-01-26 15:20:29

---
+++ BZDYM/Program.cs
@@ -0,0 +1,22 @@
+﻿//主程序入口///////////////////////////////////
+using System;
+using System.Windows.Forms;
+using BZD;
+namespace WindowsFormsApp3
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new waizai());
+
+        }
+    }
+
+}
